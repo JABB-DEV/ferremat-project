3a27fd48f191af71727c7becf4d052aa6ececdf1
Jose Alberto <pepe_jabb@hotmail.com>
2020-09-22 14:11:15 -0500

    Instalando el proyecto en forma local con composer

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\ferremat-project\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\ferremat-project\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\ferremat\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\ferremat\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
